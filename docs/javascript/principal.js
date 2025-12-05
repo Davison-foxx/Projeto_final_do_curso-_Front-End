a47eba029caff9217ff12d16f955000c6f2608dd
Davison-foxx <davisonfoxx@gmail.com>
2025-12-05 19:43:03 -0300

    adicionando a linha js no projeto e pondo edição para fazer o carrossel ficar funcional

---
+++ docs/javascript/principal.js
@@ -0,0 +1,23 @@
+//codigo que faz o  boton de slide funcionar 01
+const $next = document.querySelector('.next');
+const $prev = document.querySelector('.prev');
+
+$next.addEventListener(
+    'click', 
+    () => {
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').
+        appendChild(items[0]);
+            
+        },
+);
+
+$prev.addEventListener(
+    'click', 
+    () => {
+        const items = document.
+        querySelectorAll('.item');
+        document.querySelector('.slide').
+        prepend(items[items.length - 1]);
+    },
+);
