5c70953714d910bbfbceebe4ea7e638e945786e2
Davison-foxx <davisonfoxx@gmail.com>
2025-12-05 22:40:45 -0300

    adicionei edições com js para que o botao fique funcional

--- docs/javascript/principal.js
+++ docs/javascript/principal.js
@@ -44,3 +44,31 @@ meuLa_ele.addEventListener("click", function() {
   window.location.href = url_Destino;
 });
 //fim 02
+
+// codigo que faz o botao de rolarem para o topo funcionar 03
+const btnTop = document.getElementById("btnTop");
+
+// Configurar o estilo fixo do botão
+btnTop.style.position = "fixed";
+btnTop.style.bottom = "20px";
+btnTop.style.right = "20px";
+btnTop.style.display = "none"; // Inicialmente oculto
+
+
+// Mostrar o botão apenas quando o usuário rolar para baixo
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    btnTop.style.display = "block";
+  } else {
+    btnTopo.style.display = "none";
+  }
+};
+
+// Ao clicar, rola suavemente para o topo
+btnTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+// fim 03
