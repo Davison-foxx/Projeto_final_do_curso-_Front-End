069fbfee95b1674870cdec050f71a827489b271c
Davison-foxx <davisonfoxx@gmail.com>
2025-12-06 08:52:17 -0300

    aplicando ediçoes com css na pagina sobre no botao de minhas informaçoes

--- docs/javascript/principal.js
+++ docs/javascript/principal.js
@@ -72,3 +72,25 @@ btnTop.addEventListener("click", () => {
   });
 });
 // fim 03
+
+function mudar() {
+    let msg = document.getElementById("mensage");
+    let texto = "Olá! Meu nome é Davison Foxx, tenho 18 anos e sou um entusiasta de tecnologia e desenvolvimento web. Atualmente, estou cursando o ensino médio, onde descobri minha paixão por programação e design de sites. Além dos estudos, gosto de explorar novas tecnologias, aprender sobre tendências digitais e trabalhar em projetos pessoais que me desafiam a crescer como desenvolvedor. Estou sempre em busca de oportunidades para aprimorar minhas habilidades e contribuir para a criação de experiências web inovadoras.";
+    let info = document.getElementById("info")
+    let btnInfo = document.querySelector(".bot1")
+
+
+if (msg.innerText === "") {
+        msg.innerText = texto;
+        msg.style.color="black"
+        //info.style.backgroundImage = "url(../javascript/image/image.jpg)"
+        btnInfo.style.border = "none"
+        btnInfo.style.color = "black"
+        
+
+    } else {
+        //info.style.background = "#fff";
+        msg.innerText = "";
+        
+    }
+}
